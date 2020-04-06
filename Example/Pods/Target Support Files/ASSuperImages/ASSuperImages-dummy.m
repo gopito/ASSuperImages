bad6eabe6d70f4a3ab40a447452d37e7f96272e1
a.smolianin <a.smolianin@corp.mail.ru>
2020-04-06 23:54:19 +0300

    initial commit

---
+++ Example/Pods/Target Support Files/ASSuperImages/ASSuperImages-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ASSuperImages : NSObject
+@end
+@implementation PodsDummy_ASSuperImages
+@end
---
+++ Example/Pods/Target Support Files/Pods-ASSuperImages_Example/Pods-ASSuperImages_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASSuperImages_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ASSuperImages_Example
+@end
